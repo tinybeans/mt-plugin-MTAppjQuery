105da0297c44fc6da777e2e7ae9c1458e39b0082
tinybeans <register@tinybeans.net>
2011-04-20 11:38:11 +0900

    Add "mtappVars.screen_id"

--- plugins/MTAppjQuery/lib/MTAppjQuery/Plugin.pm
+++ plugins/MTAppjQuery/lib/MTAppjQuery/Plugin.pm
@@ -171,12 +171,13 @@ __MTML__
         "page_id" : ${page_id},
         "category_id" : ${category_id},
         "template_id" : ${template_id},
-        "blog_url" : '<mt:if name="blog_url"><mt:var name="blog_url"><mt:else><mt:var name="site_url"></mt:if>',
-        "static_plugin_path" : '${static_plugin_path}',
-        "html_title" : '<mt:if name="html_title"><mt:var name="html_title"><mt:else><mt:var name="page_title"></mt:if>',
-        "scope_type" : '<mt:var name="scope_type">',
+        "blog_url" : "<mt:if name="blog_url"><mt:var name="blog_url"><mt:else><mt:var name="site_url"></mt:if>",
+        "static_plugin_path" : "${static_plugin_path}",
+        "html_title" : "<mt:if name="html_title"><mt:var name="html_title"><mt:else><mt:var name="page_title"></mt:if>",
+        "scope_type" : "<mt:var name="scope_type">",
         "selected_category" : <mt:if name="selected_category_loop"><mt:var name="selected_category_loop" to_json="1"><mt:else>[]</mt:if>,
-        "main_category_id" : <mt:if name="category_id"><mt:var name="category_id"><mt:else>''</mt:if>
+        "main_category_id" : <mt:if name="category_id"><mt:var name="category_id"><mt:else>0</mt:if>,
+        "screen_id" : "<mt:var name="screen_id">"
     }
     /* ]]> */
     </script>
