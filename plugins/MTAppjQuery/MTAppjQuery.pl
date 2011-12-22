980c48494de912a2b1b7ea3286aef9d24e7d6e9b
tinybeans <register@tinybeans.net>
2011-12-22 09:36:44 +0900

    Bug fix : mtappVarsなどにencode_js="1"を加えた。

--- plugins/MTAppjQuery/MTAppjQuery.pl
+++ plugins/MTAppjQuery/MTAppjQuery.pl
@@ -2,7 +2,7 @@ package MT::Plugin::MTAppjQuery;
 use strict;
 use base qw( MT::Plugin );
 
-our $VERSION = '0.25';
+our $VERSION = '0.25.1';
 
 my $plugin = MT::Plugin::MTAppjQuery->new({
     id          => 'mt_app_jquery',
