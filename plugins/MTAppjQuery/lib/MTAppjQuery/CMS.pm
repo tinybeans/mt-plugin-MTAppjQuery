bf9bef1cf9a4a74a4dcad0519bd89fff03c0c700
tinybeans <register@tinybeans.net>
2012-05-22 15:50:55 +0900

    「user.jsを編集」「user.cssを編集」メニューのメソッドを追加した。

--- plugins/MTAppjQuery/lib/MTAppjQuery/CMS.pm
+++ plugins/MTAppjQuery/lib/MTAppjQuery/CMS.pm
@@ -75,4 +75,56 @@ __USER_CSS__
     $app->call_return();
 }
 
+sub edit_user_js {
+    my $app = shift;
+    my $name = MT->config->MTAppjQueryUserJSName || 'user.js';
+    my $ident = 'user_js';
+    _go_user_files($app, $name, $ident);
+}
+
+sub edit_user_css {
+    my $app = shift;
+    my $name = MT->config->MTAppjQueryUserCSSName || 'user.css';
+    my $ident = 'user_css';
+    _go_user_files($app, $name, $ident);
+}
+
+sub _go_user_files {
+    my ($app, $name, $ident) = @_;
+    my %return_params;
+    if (my $blog = $app->blog) {
+        unless (is_user_can($blog, $app->user, 'edit_templates')) {
+            return $app->trans_error('Permission denied.');
+        }
+        require MT::Template;
+        my $template = MT::Template->load({
+            name => $name,
+            identifier => $ident,
+            blog_id => $blog->id,
+        });
+        if (defined $template) {
+            %return_params = (
+                __mode => 'view',
+                _type => 'template',
+                id => $template->id,
+                blog_id => $blog->id,
+            );
+        } else {
+            %return_params = (
+                __mode => 'list_template',
+                blog_id => $blog->id,
+                no_such_template => 1, 
+            );
+        }
+        $app->add_return_arg(%return_params);
+    } else {
+        %return_params = (
+            __mode => 'list_template',
+            blog_id => $blog->id,
+            no_such_template => 1, 
+        );
+    }
+    $app->call_return();
+}
+
 1;
