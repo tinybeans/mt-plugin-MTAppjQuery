dba18db1f5484f6ea812ddc4a45e7a28b347d9f1
Tomohiro Okuwaki <register@tinybeans.net>
2017-05-12 14:39:00 +1200

    [Upgrade] Upgrade v1.10.6

--- MTAppjQuery/plugins/MTAppjQuery/MTAppjQuery.pl
+++ MTAppjQuery/plugins/MTAppjQuery/MTAppjQuery.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use base qw( MT::Plugin );
 
-our $VERSION = '1.10.5';
+our $VERSION = '1.10.6';
 
 my $plugin = MT::Plugin::MTAppjQuery->new({
     id          => 'mt_app_jquery',
