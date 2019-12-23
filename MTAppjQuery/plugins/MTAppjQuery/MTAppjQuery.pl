74d31bbe75200b82d2a0077617a59838b67f8515
Roy Okuwaki <tom.okuwaki@gmail.com>
2019-12-23 11:16:30 +0900

    upgrade: Released v1.11.7

--- MTAppjQuery/plugins/MTAppjQuery/MTAppjQuery.pl
+++ MTAppjQuery/plugins/MTAppjQuery/MTAppjQuery.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use base qw( MT::Plugin );
 
-our $VERSION = '1.11.6';
+our $VERSION = '1.11.7';
 
 my $plugin = MT::Plugin::MTAppjQuery->new({
     id          => 'mt_app_jquery',
